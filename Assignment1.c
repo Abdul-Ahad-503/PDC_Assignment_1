706b9ed1a69d9c03d86a6a30aec305d67158b7e1
Abdul_Ahad_503 <abdulahad503456@gmail.com>
2025-03-09 17:05:01 +0500

    Added parallel function for even-odd separation using OpenMP

--- Assignment1.c
+++ Assignment1.c
@@ -5,6 +5,47 @@
 
 #define N 1000  // Minimum size of the array
 
+void parallel_count_and_separate(int *arr, int size, int *evens, int *odds, int *even_count, int *odd_count, int num_threads) {
+    int local_even_count = 0, local_odd_count = 0;
+
+    // Set number of threads
+    omp_set_num_threads(num_threads);
+
+    #pragma omp parallel
+    {
+        int *private_evens = (int *)malloc(size * sizeof(int));
+        int *private_odds = (int *)malloc(size * sizeof(int));
+        int private_even_count = 0, private_odd_count = 0;
+
+        #pragma omp for nowait schedule(dynamic)
+        for (int i = 0; i < size; i++) {
+            if (arr[i] % 2 == 0) {
+                private_evens[private_even_count++] = arr[i];
+            } else {
+                private_odds[private_odd_count++] = arr[i];
+            }
+        }
+
+        // Merging local results into shared arrays
+        #pragma omp critical
+        {
+            for (int i = 0; i < private_even_count; i++) {
+                evens[local_even_count++] = private_evens[i];
+            }
+            for (int i = 0; i < private_odd_count; i++) {
+                odds[local_odd_count++] = private_odds[i];
+            }
+        }
+
+        free(private_evens);
+        free(private_odds);
+    }
+
+    *even_count = local_even_count;
+    *odd_count = local_odd_count;
+}
+
+
 int main(int argc, char *argv[]) {
     srand(time(0));
     int arr[N];
